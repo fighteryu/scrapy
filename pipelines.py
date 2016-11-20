9433321be9745801861046316bad4dd359143f8a
Hunter Yu <zgzjjhyh@126.com>
2016-11-20 14:10:39 +0800

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -5,10 +5,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+#piplines 文件用于数据的后处理，需要开启setting文件的piplines
 class FirstPipeline(object):
     def process_item(self, item, spider):
-        #print(item["title"])
         for i in range(0,len(item["title"])):
             print("第"+str(i+1)+"篇文章：")
             print(item["title"][i])
